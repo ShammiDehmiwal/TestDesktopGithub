5ad9ca4cfd9759e0315f114692ef5de5941c66dc
ShammiDehmiwal <shammi@rnaura.com>
2019-04-30 15:05:07 +0530

    Addition of msg add show
    just inputing message from user and showing to screen.

--- TestDesktopGithub/ViewController.swift
+++ TestDesktopGithub/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMsg: UILabel!
+    
+    @IBOutlet weak var txtMsg: UITextField!
+    
+    //MARK: - Life Cycle Methods.
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //MARK: - UIButton Action Methods.
+    @IBAction func btnClickTapAction(_ sender: UIButton)
+    {
+        lblMsg.text = txtMsg.text
+        
+        txtMsg.text = ""
+    }
+    
 }
 
